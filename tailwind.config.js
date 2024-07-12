c24259942e76e647806649c68eb3eb5be42a5cec
ralptorres <ralp.torres@gmail.com>
2024-07-12 22:36:05 +0800

    define screens, add media query, fix layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
       },
+      screens: {
+        ss: "0px",
+        sm: "640px", // Small screens (default: 640px)
+        md: "768px", // Medium screens (default: 768px)
+        lg: "1024px", // Large screens (default: 1024px)
+        xl: "1280px", // Extra large screens (default: 1280px)
+      },
     },
   },
   plugins: [],
